cc8367bddc557c518401c2dd03e295a5da80c8ab
zrthstr <zrth1k@gmail.com>
2020-05-23 21:29:12 +0200

    ..

--- func_basic.clj
+++ func_basic.clj
@@ -1,10 +1,14 @@
-
 ( ns func )
 
-( defn somefunc
-  ( [] ( print "no arg\n"))
-  ( [first] (print (str "one arg:" first "\n"))))
+( defn no_arg_func
+  ( [] ( print "no arg\n")))
 
+( defn one_arg_func
+  ( [first] (print (str "one arg: " first "\n"))))
 
-( somefunc )
-( somefunc "some_arg")
+( defn rand_arg_func [& some]
+  ( print (str "numb of args: ", (count some) "\n")))
+
+( no_arg_func )
+( one_arg_func "some_arg")
+( rand_arg_func 0 1 2 4 5 6)
