549a9dd7232e488f2ae614b67944cd6c2a3ae4c2
zrthstr <zrth1k@gmail.com>
2020-05-28 23:41:11 +0200

    ..

--- types.clj
+++ types.clj
@@ -1,6 +1,13 @@
 
 (ns types)
 
+(println (type true))
+(println (type 10))
+(println (type 1.1))
+(println (type [1 2 3]))
+
+
 (println (= true true))
 (println (= true false))
 (println (not true))
+
