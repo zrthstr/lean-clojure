680d2d3f9ce22165316f42a48a0dc24d67799038
zrthstr <zrth1k@gmail.com>
2020-05-28 23:45:06 +0200

    ..

--- types.clj
+++ types.clj
@@ -4,7 +4,17 @@
 (println (type true))
 (println (type 10))
 (println (type 1.1))
+
+(println (type "1234"))
+
 (println (type [1 2 3]))
+(println (type '(11 22 33)))
+(println (type :a ))
+
+(println (type Thread))
+(println (type 'blabla))
+(println (type clojure.lang.Symbol))
+
 
 
 (println (= true true))
