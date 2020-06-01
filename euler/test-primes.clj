97abd6f20a91ddfa4daa44d7adc8d18ea2ab2959
zrthstr <zrth1k@gmail.com>
2020-06-01 23:43:00 +0200

    primes lib tests

---
+++ euler/test-primes.clj
@@ -0,0 +1,13 @@
+
+( ns prime-test )
+
+(load-file '"prime.clj")
+
+(println (take 5 (primes/gen-primes)))
+(println (primes/n-primes 10))
+(println (primes/to-primes 100))
+
+(println (primes/is-prime 13))
+(println (primes/is-prime 31))
+(println (primes/is-prime 44))
+(println (primes/is-prime 10))
